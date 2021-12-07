719a6e7e0b733722f137418b1f80b037388aded0
gauravkgupta <gaurav.k.gupta@ukg.com>
2021-12-07 10:44:25 +0530

    test commit

--- controller/security.go
+++ controller/security.go
@@ -6,16 +6,15 @@ type Security struct {
 	beego.Controller
 }
 
-func (s *Security) Login()  {
+//comment
+func (s *Security) Login() {
 	s.Ctx.ResponseWriter.Write([]byte("login controller"))
 }
 
-func (s *Security) Logout()  {
+func (s *Security) Logout() {
 	s.Ctx.ResponseWriter.Write([]byte("logout controller"))
 }
 
-
-func (s *Security) Authenticate()  {
+func (s *Security) Authenticate() {
 	s.Ctx.ResponseWriter.Write([]byte("authenticate controller"))
 }
-
